9bafaf1bb745e39d0845b258dcfd48d66a83b5ec
3dPlease <73364162+3dPlease@users.noreply.github.com>
2020-11-05 11:49:02 -0500

    2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import * as THREE from 'build/three.module.js';
-import { OrbitControls } from 'jsm/OrbitControls.js';
+import * as THREE from './build/three.module.js';
+import { OrbitControls } from './jsm/OrbitControls.js';
 
 class App{
 	constructor(){
